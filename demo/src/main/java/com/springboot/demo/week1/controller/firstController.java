6938a50c51540fddc043fe9d304e1ab86d907f54
HieuNC7 <hieunc.10390@gmail.com>
2023-05-18 01:22:20 +0700

    week 1

--- demo/src/main/java/com/springboot/demo/week1/controller/firstController.java
+++ demo/src/main/java/com/springboot/demo/week1/controller/firstController.java
@@ -1,11 +1,13 @@
 package com.springboot.demo.week1.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/rest-api")
 public class firstController {
-    @GetMapping("/")
+    @GetMapping("/first-api")
     public String firstRestApi(){
         return "Hello World!";
     }
